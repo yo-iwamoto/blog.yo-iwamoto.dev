dd57928082491e1667129a848e1267b8a7e926b7
Yo Iwamoto <you.iwamoto918@gmail.com>
2025-06-07 12:44:33 +0900

    rm unused envs

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,17 +2,11 @@ import { cache } from "react";
 import { z } from "zod";
 
 const serverSideEnvSchema = z.object({
-  newtCdnApiToken: z.string(),
-  newtSpaceUid: z.string(),
-  newtAppUid: z.string(),
   websiteUrl: z.string(),
 });
 
 function getServerSideEnvFn() {
   return serverSideEnvSchema.parse({
-    newtCdnApiToken: process.env.NEWT_CDN_API_TOKEN,
-    newtSpaceUid: process.env.NEWT_SPACE_UID,
-    newtAppUid: process.env.NEWT_APP_UID,
     websiteUrl: process.env.WEBSITE_URL,
   });
 }
